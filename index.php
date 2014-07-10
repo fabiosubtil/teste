82e93b657f1093fde7f63f4d2217b6769f9ee154
fabiosubtil <fabiosubtil@tre-rs.jus.br>
2014-07-10 14:41:44 -0300

    Quarta modificação no arquivo.

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ and open the template in the editor.
       echo 'Primeiro GitHub.';     
       echo 'Segundo GitHub.';     
       echo 'Terceiro GitHub.';     
+      echo 'Quarto GitHub.';
         ?>
     </body>
 </html>
